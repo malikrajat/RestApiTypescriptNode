e6b78a08072e734596278a722b22bf6cda534bf5
malikrajat <er.rajatmalik@gmail.com>
2021-10-24 18:31:33 +0530

    createUser

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,7 +1,11 @@
 import { DocumentDefinition } from "mongoose";
 import UserModel, { UserDocument } from "../models/user.Model";
 
-const createUser = async (input: DocumentDefinition<UserDocument>) => {
+const createUser = async (
+	input: DocumentDefinition<
+		Omit<UserDocument, "createdAt" | "updatedAt" | "comparPassword">
+	>
+) => {
 	try {
 		return await UserModel.create(input);
 	} catch (error: any) {
