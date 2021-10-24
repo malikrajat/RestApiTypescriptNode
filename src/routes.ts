2714110f87a8a838a8421f4e39cffc5c0af21f7c
malikrajat <er.rajatmalik@gmail.com>
2021-10-24 18:31:04 +0530

    uses route

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,6 @@ import createUserSchema from "./schema/user.schema";
 
 const routes = (app: Express) => {
 	app.get("/health", (req: Request, res: Response) => res.sendStatus(200));
-	app.post("api/users", validate(createUserSchema), createUserHandler);
+	app.post("/api/users", validate(createUserSchema), createUserHandler);
 };
 export default routes;
