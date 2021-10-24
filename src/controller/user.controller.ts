a8f78641225fd1d81219efb1cfb612c63dc8052e
malikrajat <er.rajatmalik@gmail.com>
2021-10-24 20:32:40 +0530

    user controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import createUser from "../service/user.service";
 import logger from "../utils/logger";
 import { CreateUserInput } from "../schema/user.schema";
+import { omit } from "lodash";
 
 const createUserHandler = async (
 	req: Request<{}, {}, CreateUserInput["body"]>,
@@ -9,7 +10,7 @@ const createUserHandler = async (
 ) => {
 	try {
 		const user = await createUser(req.body);
-		return user;
+		return res.send(omit(user.toJSON(), "password"));
 	} catch (error: any) {
 		return logger.error(error);
 		return res.status(409).send(error.message);
